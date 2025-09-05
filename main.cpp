e035af19caeefcb2ac1e03a6bf7464aaff0059bf
rlxlxl <slavavik1337@mail.ru>
2025-09-06 01:52:50 +0700

    Изменен main

--- main.cpp
+++ main.cpp
@@ -6,6 +6,22 @@ using namespace std;
 
 int main() {
 
-    createTable();
-    deleteTable();
+    while (true) {
+        cout << "1. Создать таблицу" << endl;
+        cout << "2. Удалить таблицу" << endl;
+        cout << "3. Выход" << endl;
+
+        int choice;
+        cin >> choice;
+
+        if (choice == 1) {
+            createTable();
+        } else if (choice == 2) {
+            deleteTable();
+        } else {
+            break;
+        }
+    }
+
+    return 0;
 }
